97d361aecc2142d96e915a625392852f83d949ad
RNGSSB <59853617+RNGSSB@users.noreply.github.com>
2024-02-08 13:45:04 -0600

    Quick Samus

--- skyline-rs-template/src/samus/acmd/aerials.rs
+++ skyline-rs-template/src/samus/acmd/aerials.rs
@@ -8,9 +8,17 @@ use {
     smash_script::*
 };
 
+unsafe extern "C" fn game_aircatchlanding(agent: &mut L2CAgentBase) {
+    frame(agent.lua_state_agent, 1.0);
+    macros::FT_MOTION_RATE(agent, 0.88);
+    if macros::is_excute(agent) {
+        WorkModule::on_flag(agent.module_accessor, *FIGHTER_SAMUS_INSTANCE_WORK_ID_FLAG_ARTICLE_MOTION_RATE_SYNC);
+    }
+}
+
 
 pub fn install(fighter: &mut smashline::Agent) {
-    //fighter.game_acmd("game_attackairb", game_attackairb);
+    fighter.game_acmd("game_aircatchlanding", game_aircatchlanding);
     //fighter.effect_acmd("effect_attackairf", effect_attackairf);
     //fighter.sound_acmd("sound_attackairf", sound_attackairf);
 }
--- skyline-rs-template/src/samus/frame.rs
+++ skyline-rs-template/src/samus/frame.rs
@@ -10,9 +10,24 @@ use {
 use smash::app::utility::get_kind;
 
 
+pub unsafe fn landCancels(boma: &mut smash::app::BattleObjectModuleAccessor, status_kind: i32, situation_kind: i32, fighter_kind: i32) { //Fox & Falco Lasers
+    if [*FIGHTER_KIND_SAMUS].contains(&fighter_kind) {
+        if status_kind == *FIGHTER_STATUS_KIND_SPECIAL_S {
+            if StatusModule::prev_situation_kind(boma) == *SITUATION_KIND_AIR && situation_kind == *SITUATION_KIND_GROUND {
+                StatusModule::change_status_request_from_script(boma, *FIGHTER_STATUS_KIND_LANDING, true);
+            }
+        }
+    }
+}
+
 // What used to be known as a "Once-Per-Fighter-Frame". On-Line functions can be set to run on any status condition.
 unsafe extern "C" fn char_on_main(fighter: &mut L2CFighterCommon) {
     unsafe {
+        let module_accessor = &mut *fighter.module_accessor;
+        let status_kind = StatusModule::status_kind(fighter.module_accessor);
+        let fighter_kind = get_kind(module_accessor);
+        let situation_kind = StatusModule::situation_kind(module_accessor);
+        landCancels(module_accessor, status_kind, situation_kind, fighter_kind);
         global_fighter_frame(fighter);
     }
 }
