8d81556c0b3dd4dc670391889e92e3d70570a452
RNGSSB <59853617+RNGSSB@users.noreply.github.com>
2024-04-18 20:36:28 -0600

    it can't be that easy

--- skyline-rs-template/src/sora/status/special_lw.rs
+++ skyline-rs-template/src/sora/status/special_lw.rs
@@ -13,7 +13,15 @@ use super::*;
 use smashline::Pre;
 use smash::lib::{L2CValue, L2CAgent};
 
+pub unsafe extern "C" fn attack_air_n_init(fighter: &mut L2CFighterCommon) -> L2CValue {
+    0.into()
+}
+
+pub unsafe extern "C" fn attack_air_f_init(fighter: &mut L2CFighterCommon) -> L2CValue {
+    0.into()
+}
 
 pub fn install(agent: &mut smashline::Agent) {
-    //agent.status(smashline::Main, *FIGHTER_STATUS_KIND_SPECIAL_LW, special_lw_init);
+    agent.status(Init, *FIGHTER_TRAIL_STATUS_KIND_ATTACK_AIR_N, attack_air_n_init);
+    agent.status(Init, *FIGHTER_TRAIL_STATUS_KIND_ATTACK_AIR_F, attack_air_f_init);
 }
