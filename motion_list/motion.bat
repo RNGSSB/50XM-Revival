4fdf1c86b08dc296486f92f58b4e14690c06ef9d
RNGSSB <59853617+RNGSSB@users.noreply.github.com>
2024-04-24 20:58:12 -0600

    deleted previous yaml

--- motion_list/motion.bat
+++ motion_list/motion.bat
@@ -1,8 +1,99 @@
+yamlist.exe asm "yaml\fox.yaml" -o fighter/fox/motion_list.bin
+yamlist.exe asm "yaml\ridley.yaml" -o fighter/ridley/motion_list.bin
+yamlist.exe asm "yaml\cloud.yaml" -o fighter/cloud/motion_list.bin
+yamlist.exe asm "yaml\wolf.yaml" -o fighter/wolf/motion_list.bin
+yamlist.exe asm "yaml\mario.yaml" -o fighter/mario/motion_list.bin
+yamlist.exe asm "yaml\captain.yaml" -o fighter/captain/motion_list.bin
+yamlist.exe asm "yaml\sheik.yaml" -o fighter/sheik/motion_list.bin
+yamlist.exe asm "yaml\falco.yaml" -o fighter/falco/motion_list.bin
+yamlist.exe asm "yaml\snake.yaml" -o fighter/snake/motion_list.bin
 yamlist.exe asm "yaml\lucario.yaml" -o fighter/lucario/motion_list.bin
 yamlist.exe asm "yaml\ness.yaml" -o fighter/ness/motion_list.bin
+yamlist.exe asm "yaml\elight.yaml" -o fighter/elight/motion_list.bin
 yamlist.exe asm "yaml\luigi.yaml" -o fighter/luigi/motion_list.bin
 
 cd..
+
+copy /y "motion_list\fighter\fox\motion_list.bin" "50XM\fighter\fox\motion\body\c00"
+copy /y "motion_list\fighter\fox\motion_list.bin" "50XM\fighter\fox\motion\body\c01"
+copy /y "motion_list\fighter\fox\motion_list.bin" "50XM\fighter\fox\motion\body\c02"
+copy /y "motion_list\fighter\fox\motion_list.bin" "50XM\fighter\fox\motion\body\c03"
+copy /y "motion_list\fighter\fox\motion_list.bin" "50XM\fighter\fox\motion\body\c04"
+copy /y "motion_list\fighter\fox\motion_list.bin" "50XM\fighter\fox\motion\body\c05"
+copy /y "motion_list\fighter\fox\motion_list.bin" "50XM\fighter\fox\motion\body\c06"
+copy /y "motion_list\fighter\fox\motion_list.bin" "50XM\fighter\fox\motion\body\c07"
+
+copy /y "motion_list\fighter\mario\motion_list.bin" "50XM\fighter\mario\motion\body\c00"
+copy /y "motion_list\fighter\mario\motion_list.bin" "50XM\fighter\mario\motion\body\c01"
+copy /y "motion_list\fighter\mario\motion_list.bin" "50XM\fighter\mario\motion\body\c02"
+copy /y "motion_list\fighter\mario\motion_list.bin" "50XM\fighter\mario\motion\body\c03"
+copy /y "motion_list\fighter\mario\motion_list.bin" "50XM\fighter\mario\motion\body\c04"
+copy /y "motion_list\fighter\mario\motion_list.bin" "50XM\fighter\mario\motion\body\c05"
+copy /y "motion_list\fighter\mario\motion_list.bin" "50XM\fighter\mario\motion\body\c06"
+copy /y "motion_list\fighter\mario\motion_list.bin" "50XM\fighter\mario\motion\body\c07"
+
+copy /y "motion_list\fighter\wolf\motion_list.bin" "50XM\fighter\wolf\motion\body\c00"
+copy /y "motion_list\fighter\wolf\motion_list.bin" "50XM\fighter\wolf\motion\body\c01"
+copy /y "motion_list\fighter\wolf\motion_list.bin" "50XM\fighter\wolf\motion\body\c02"
+copy /y "motion_list\fighter\wolf\motion_list.bin" "50XM\fighter\wolf\motion\body\c03"
+copy /y "motion_list\fighter\wolf\motion_list.bin" "50XM\fighter\wolf\motion\body\c04"
+copy /y "motion_list\fighter\wolf\motion_list.bin" "50XM\fighter\wolf\motion\body\c05"
+copy /y "motion_list\fighter\wolf\motion_list.bin" "50XM\fighter\wolf\motion\body\c06"
+copy /y "motion_list\fighter\wolf\motion_list.bin" "50XM\fighter\wolf\motion\body\c07"
+
+copy /y "motion_list\fighter\cloud\motion_list.bin" "50XM\fighter\cloud\motion\body\c00"
+copy /y "motion_list\fighter\cloud\motion_list.bin" "50XM\fighter\cloud\motion\body\c01"
+copy /y "motion_list\fighter\cloud\motion_list.bin" "50XM\fighter\cloud\motion\body\c02"
+copy /y "motion_list\fighter\cloud\motion_list.bin" "50XM\fighter\cloud\motion\body\c03"
+copy /y "motion_list\fighter\cloud\motion_list.bin" "50XM\fighter\cloud\motion\body\c04"
+copy /y "motion_list\fighter\cloud\motion_list.bin" "50XM\fighter\cloud\motion\body\c05"
+copy /y "motion_list\fighter\cloud\motion_list.bin" "50XM\fighter\cloud\motion\body\c06"
+copy /y "motion_list\fighter\cloud\motion_list.bin" "50XM\fighter\cloud\motion\body\c07"
+
+copy /y "motion_list\fighter\ridley\motion_list.bin" "50XM\fighter\ridley\motion\body\c00"
+copy /y "motion_list\fighter\ridley\motion_list.bin" "50XM\fighter\ridley\motion\body\c01"
+copy /y "motion_list\fighter\ridley\motion_list.bin" "50XM\fighter\ridley\motion\body\c02"
+copy /y "motion_list\fighter\ridley\motion_list.bin" "50XM\fighter\ridley\motion\body\c03"
+copy /y "motion_list\fighter\ridley\motion_list.bin" "50XM\fighter\ridley\motion\body\c04"
+copy /y "motion_list\fighter\ridley\motion_list.bin" "50XM\fighter\ridley\motion\body\c05"
+copy /y "motion_list\fighter\ridley\motion_list.bin" "50XM\fighter\ridley\motion\body\c06"
+copy /y "motion_list\fighter\ridley\motion_list.bin" "50XM\fighter\ridley\motion\body\c07"
+
+copy /y "motion_list\fighter\captain\motion_list.bin" "50XM\fighter\captain\motion\body\c00"
+copy /y "motion_list\fighter\captain\motion_list.bin" "50XM\fighter\captain\motion\body\c01"
+copy /y "motion_list\fighter\captain\motion_list.bin" "50XM\fighter\captain\motion\body\c02"
+copy /y "motion_list\fighter\captain\motion_list.bin" "50XM\fighter\captain\motion\body\c03"
+copy /y "motion_list\fighter\captain\motion_list.bin" "50XM\fighter\captain\motion\body\c04"
+copy /y "motion_list\fighter\captain\motion_list.bin" "50XM\fighter\captain\motion\body\c05"
+copy /y "motion_list\fighter\captain\motion_list.bin" "50XM\fighter\captain\motion\body\c06"
+copy /y "motion_list\fighter\captain\motion_list.bin" "50XM\fighter\captain\motion\body\c07"
+
+copy /y "motion_list\fighter\sheik\motion_list.bin" "50XM\fighter\sheik\motion\body\c00"
+copy /y "motion_list\fighter\sheik\motion_list.bin" "50XM\fighter\sheik\motion\body\c01"
+copy /y "motion_list\fighter\sheik\motion_list.bin" "50XM\fighter\sheik\motion\body\c02"
+copy /y "motion_list\fighter\sheik\motion_list.bin" "50XM\fighter\sheik\motion\body\c03"
+copy /y "motion_list\fighter\sheik\motion_list.bin" "50XM\fighter\sheik\motion\body\c04"
+copy /y "motion_list\fighter\sheik\motion_list.bin" "50XM\fighter\sheik\motion\body\c05"
+copy /y "motion_list\fighter\sheik\motion_list.bin" "50XM\fighter\sheik\motion\body\c06"
+copy /y "motion_list\fighter\sheik\motion_list.bin" "50XM\fighter\sheik\motion\body\c07"
+
+copy /y "motion_list\fighter\falco\motion_list.bin" "50XM\fighter\falco\motion\body\c00"
+copy /y "motion_list\fighter\falco\motion_list.bin" "50XM\fighter\falco\motion\body\c01"
+copy /y "motion_list\fighter\falco\motion_list.bin" "50XM\fighter\falco\motion\body\c02"
+copy /y "motion_list\fighter\falco\motion_list.bin" "50XM\fighter\falco\motion\body\c03"
+copy /y "motion_list\fighter\falco\motion_list.bin" "50XM\fighter\falco\motion\body\c04"
+copy /y "motion_list\fighter\falco\motion_list.bin" "50XM\fighter\falco\motion\body\c05"
+copy /y "motion_list\fighter\falco\motion_list.bin" "50XM\fighter\falco\motion\body\c06"
+copy /y "motion_list\fighter\falco\motion_list.bin" "50XM\fighter\falco\motion\body\c07"
+
+copy /y "motion_list\fighter\snake\motion_list.bin" "50XM\fighter\snake\motion\body\c00"
+copy /y "motion_list\fighter\snake\motion_list.bin" "50XM\fighter\snake\motion\body\c01"
+copy /y "motion_list\fighter\snake\motion_list.bin" "50XM\fighter\snake\motion\body\c02"
+copy /y "motion_list\fighter\snake\motion_list.bin" "50XM\fighter\snake\motion\body\c03"
+copy /y "motion_list\fighter\snake\motion_list.bin" "50XM\fighter\snake\motion\body\c04"
+copy /y "motion_list\fighter\snake\motion_list.bin" "50XM\fighter\snake\motion\body\c05"
+copy /y "motion_list\fighter\snake\motion_list.bin" "50XM\fighter\snake\motion\body\c06"
+copy /y "motion_list\fighter\snake\motion_list.bin" "50XM\fighter\snake\motion\body\c07"
 
 copy /y "motion_list\fighter\lucario\motion_list.bin" "50XM\fighter\lucario\motion\body\c00"
 copy /y "motion_list\fighter\lucario\motion_list.bin" "50XM\fighter\lucario\motion\body\c01"
@@ -21,6 +112,16 @@ copy /y "motion_list\fighter\ness\motion_list.bin" "50XM\fighter\ness\motion\bod
 copy /y "motion_list\fighter\ness\motion_list.bin" "50XM\fighter\ness\motion\body\c05"
 copy /y "motion_list\fighter\ness\motion_list.bin" "50XM\fighter\ness\motion\body\c06"
 copy /y "motion_list\fighter\ness\motion_list.bin" "50XM\fighter\ness\motion\body\c07"
+
+copy /y "motion_list\fighter\elight\motion_list.bin" "50XM\fighter\elight\motion\body\c00"
+copy /y "motion_list\fighter\elight\motion_list.bin" "50XM\fighter\elight\motion\body\c01"
+copy /y "motion_list\fighter\elight\motion_list.bin" "50XM\fighter\elight\motion\body\c02"
+copy /y "motion_list\fighter\elight\motion_list.bin" "50XM\fighter\elight\motion\body\c03"
+copy /y "motion_list\fighter\elight\motion_list.bin" "50XM\fighter\elight\motion\body\c04"
+copy /y "motion_list\fighter\elight\motion_list.bin" "50XM\fighter\elight\motion\body\c05"
+copy /y "motion_list\fighter\elight\motion_list.bin" "50XM\fighter\elight\motion\body\c06"
+copy /y "motion_list\fighter\elight\motion_list.bin" "50XM\fighter\elight\motion\body\c07"
+
 
 copy /y "motion_list\fighter\luigi\motion_list.bin" "50XM\fighter\luigi\motion\body\c00"
 copy /y "motion_list\fighter\luigi\motion_list.bin" "50XM\fighter\luigi\motion\body\c01"
